043af69b114d1f034c8693c5360982c01b343b74
Martin Rognerud <mrognerud@graphcore.ai>
2020-09-23 14:57:03 +0200

    add first run at facker mode

---
+++ facker-mode.el
@@ -0,0 +1,36 @@
+(defvar facker-map  (let ((map (make-sparse-keymap)))
+    (suppress-keymap map t)
+    (define-key map (kbd "i") 'previous-line)
+    (define-key map (kbd "k") 'next-line)
+    (define-key map (kbd "j") 'left-char)
+    (define-key map (kbd "l") 'right-char)
+    (define-key map (kbd "<SPC>") 'facker-disable-map)
+    map)
+  "key map in active facker mode")
+
+(defvar facker-activate nil "controlling the facker mode map")
+
+(defun facker-disable-map ()
+    (interactive)
+    (facker-mode nil))
+
+(define-minor-mode facker-local-mode
+  "set up verilog minor mode"
+  :lighter " f"
+
+(define-globalized-minor-mode facker-mode
+  facker-local-mode
+  (lambda () (facker-local-mode t)))
+
+(provide 'facker-local-mode)
+(provide 'facker-mode)
+
+;; add the facker map to the
+(add-to-list 'emulation-mode-map-alists `((facker-local-mode . ,facker-map))))
+
+;; Turn off the minor mode in the minibuffer
+(defun facker-turn-off-local-mode ()
+  "Turn off my-mode."
+  (facker-local-mode -1))
+
+(add-hook 'minibuffer-setup-hook #'facker-turn-off-local-mode)
