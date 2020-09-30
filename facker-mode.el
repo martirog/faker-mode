310a80a6fdaebd1677e32e4c22092633569963cf
Martin Rognerud <mrogneru@yahoo.no>
2020-09-30 21:37:49 +0200

    add more keys for testing pluss insert metod for inserting text without leaving faker mode

--- facker-mode.el
+++ facker-mode.el
@@ -5,12 +5,31 @@
     (define-key map (kbd "j") 'left-char)
     (define-key map (kbd "l") 'right-char)
 
-    (define-key map (kbd "C-i") 'backward-paragraph)
-    (define-key map (kbd "C-k") 'forward-paragraph)
-    (define-key map (kbd "C-j") 'left-word)
-    (define-key map (kbd "C-l") 'right-word)
+    (define-key map (kbd "I") 'backward-paragraph)
+    (define-key map (kbd "K") 'forward-paragraph)
+    (define-key map (kbd "J") 'left-word)
+    (define-key map (kbd "L") 'right-word)
 
-    (define-key map (kbd "<SPC>") 'facker-disable-map)
+    (define-key map (kbd "<SPC> i") 'windmove-up)
+    (define-key map (kbd "<SPC> k") 'windmove-down)
+    (define-key map (kbd "<SPC> j") 'windmove-left)
+    (define-key map (kbd "<SPC> l") 'windmove-right)
+
+    (define-key map (kbd "h") 'facker-insert-text)
+
+    (define-key map (kbd "w") 'diff-buffer-with-file)
+    (define-key map (kbd "d") 'kill-line)
+    (define-key map (kbd "q") 'quit-window)
+    (define-key map (kbd "a") 'mark-word)
+    (define-key map (kbd "r") 'string-rectangle)
+
+    (define-key map (kbd "z") 'vc-print-log)
+    (define-key map (kbd "x") 'vc-print-root-log)
+    (define-key map (kbd "c") 'vc-annotate)
+    (define-key map (kbd "v") 'vc-dir)
+    (define-key map (kbd "b") 'vc-ediff)
+
+    (define-key map (kbd "<return>") 'facker-disable-map)
     map)
   "key map in active facker mode")
 
@@ -20,16 +39,20 @@
     (interactive)
     (facker-mode -1))
 
+(defun facker-insert-text (text)
+  (interactive "s")
+  (insert text))
+
 (define-minor-mode facker-local-mode
   "set up verilog minor mode"
   :lighter " f"
 
+  ;; add the facker map to the
+  (add-to-list 'emulation-mode-map-alists `((facker-local-mode . ,facker-map))))
+
 (define-globalized-minor-mode facker-mode
   facker-local-mode
   (lambda () (facker-local-mode t)))
-
-;; add the facker map to the
-(add-to-list 'emulation-mode-map-alists `((facker-local-mode . ,facker-map))))
 
 ;; Turn off the minor mode in the minibuffer
 (defun facker-turn-off-local-mode ()
